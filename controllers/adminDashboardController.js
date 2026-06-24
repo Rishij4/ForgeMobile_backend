cd7e86decdc7ebc8f56aa190046c2b45a5ca6f06
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-06-24 20:49:59 +0530

    Add files via upload

---
+++ controllers/adminDashboardController.js
@@ -0,0 +1,66 @@
+import User from "../models/User.js";
+import Build from "../models/Build.js";
+
+export const getAdminStats = async (req, res) => {
+  try {
+    const totalUsers = await User.countDocuments();
+    const totalBuilds = await Build.countDocuments();
+
+    res.json({
+      totalUsers,
+      totalBuilds
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+
+    const usersWithBuilds = await Promise.all(
+      users.map(async (user) => {
+        const buildCount = await Build.countDocuments({
+          user: user._id
+        });
+
+        return {
+          ...user._doc,
+          buildCount
+        };
+      })
+    );
+
+    res.json(usersWithBuilds);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await User.findByIdAndDelete(id);
+
+    await Build.deleteMany({
+      user: id
+    });
+
+    res.json({
+      message: "User deleted successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
