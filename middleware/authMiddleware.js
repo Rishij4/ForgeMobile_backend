e93626e37634178eb67fffcde58f357070a2e241
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-06-25 10:21:48 +0530

    Enhance authMiddleware with logging
    Added logging for headers, token, decoded user, and errors in authMiddleware.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,22 +3,30 @@ import User from "../models/User.js";
 
 const authMiddleware = async (req, res, next) => {
   try {
+    console.log("HEADERS =", req.headers);
+
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
+      console.log("NO AUTH HEADER");
       return res.status(401).json({
         message: "No token"
       });
     }
 
     const token = authHeader.split(" ")[1];
+    console.log("TOKEN =", token);
 
     const decoded = jwt.verify(
       token,
       process.env.JWT_SECRET
     );
 
+    console.log("DECODED =", decoded);
+
     const user = await User.findById(decoded.id);
+
+    console.log("USER =", user);
 
     if (!user) {
       return res.status(404).json({
@@ -31,6 +39,8 @@ const authMiddleware = async (req, res, next) => {
     next();
 
   } catch (error) {
+    console.log("AUTH ERROR =", error);
+
     return res.status(401).json({
       message: "Invalid token"
     });
